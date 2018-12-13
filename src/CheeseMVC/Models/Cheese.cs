6847eb3744d9d4d4a4d5c553729fc3a5833da5f5
Ryan <hayes.ryan95@gmail.com>
2018-12-12 18:39:55 -0600

    Still in Part I.  I just finished everything before "Adding Navigation Links".  The last thing I did was getting category functionality implemented.

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -5,6 +5,11 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public CheeseType Type { get; set; }
-        public int ID { get; set; }
+        public int ID { get; set; } //This was here to begin with.  I added the below material
+                                    //to match the video.  
+
+        public int CategoryID { get; set; }
+        public CheeseCategory Category { get; set; }
+
     }
 }
---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        // everything below has not been asked of me to do yet, but it's in the video
+
+        //public string Description { get; set; }
+       // public int CategoryID { get; set; }
+        // public CheeseCategory Category { get; set; } (REALLY IFFY ON WHY I PUT THIS ONE HERE)
+
+        //public IList<CheeseCategory> Categories { get; set; }
+
+       // public IList<Cheese> Cheeses { get; set; }
+    }
+}
