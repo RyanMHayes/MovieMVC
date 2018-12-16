70de48c8a4ece11a7049695b0cfcb45db9d07113
Ryan <hayes.ryan95@gmail.com>
2018-12-16 13:52:41 -0600

    Just finished "List Menus" task of part 3.  I am about to start "Display the Add Menu Form" task.

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(MenuController dbContext)
+        {
+            context = dbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            IList<Menu> menus = context.Menus.ToList();
+
+            return View();
+        }
+    }
+}
--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -1,4 +1,6 @@
-﻿using CheeseMVC.Models;
+﻿using System;
+using CheeseMVC.Controllers;
+using CheeseMVC.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Data
@@ -8,9 +10,22 @@ namespace CheeseMVC.Data
         public DbSet<Cheese> Cheeses { get; set; }
         public DbSet<CheeseCategory> Categories { get; set; }
 
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<CheeseMenu> CheeseMenus { get; set; }
+
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
         { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CheeseMenu>()
+                .HasKey(c => new { c.CheeseID, c.MenuID });
+        }
+
+        public static implicit operator CheeseDbContext(MenuController v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
