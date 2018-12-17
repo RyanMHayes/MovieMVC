d7cb07832c7db682843118db256f5cf56e5209d6
Ryan <hayes.ryan95@gmail.com>
2018-12-17 11:25:12 -0600

    Everything is complete prior to "Clean Up the Navigation" in part 3.

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -78,5 +78,50 @@ namespace CheeseMVC.Controllers
 
             return View(viewMenuViewModel);
         }
+
+
+        public IActionResult AddItem (int id)
+        {
+            Menu menu = context.Menus.Single(m => m.ID == id); //Check this method.  Do I need more?
+            List<Cheese> cheeses = context.Cheeses.ToList();
+            return View(new AddMenuItemViewModel(menu, cheeses));
+
+        }
+
+
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cheeseID = addMenuItemViewModel.CheeseID;
+                var menuID = addMenuItemViewModel.MenuID;
+
+                IList<CheeseMenu> existingItems = context.CheeseMenus
+                    .Where(cm => cm.CheeseID == cheeseID)
+                    .Where(cm => cm.MenuID == menuID).ToList();
+
+                if (existingItems.Count == 0)
+                {
+                    CheeseMenu menuItem = new CheeseMenu
+                    {
+                        Cheese = context.Cheeses.Single(c => c.ID == cheeseID),
+                    };
+
+                    context.CheeseMenus.Add(menuItem);
+                    context.SaveChanges();
+                }
+
+                return Redirect(string.Format("/Menu/ViewMenu/{0}", addMenuItemViewModel.Menu.ID)); 
+
+            }
+
+            return View(addMenuItemViewModel);
+
+        }
+
     }
+
+
+    
 }
