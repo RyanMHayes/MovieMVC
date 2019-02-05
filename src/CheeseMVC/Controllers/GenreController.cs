7feabce7d232d6873456aeaa922e27eac3b969b5
Ryan <hayes.ryan95@gmail.com>
2019-02-05 11:05:20 -0600

    Better user navigation in /Add of both Genre and Movie paths.  (only slight changes)

--- src/CheeseMVC/Controllers/GenreController.cs
+++ src/CheeseMVC/Controllers/GenreController.cs
@@ -48,7 +48,7 @@ namespace MovieMVC.Controllers
                 context.Genres.Add(newGenre);
                 context.SaveChanges();
 
-                return Redirect("/Movie"); //******needs to redirect to add entry page
+                return Redirect("/Movie/Add"); //******needs to redirect to add entry page
             }
 
             return View(addGenreViewModel);
